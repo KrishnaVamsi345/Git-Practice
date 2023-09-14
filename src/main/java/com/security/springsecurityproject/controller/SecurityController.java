fec420c954fd2739cc886031a66043c91fbd9fa5
KrishnaVamsi345 <krishnavamsi.mugi@temenos.com>
2023-09-14 17:24:55 +0530

    2nd commit

--- src/main/java/com/security/springsecurityproject/controller/SecurityController.java
+++ src/main/java/com/security/springsecurityproject/controller/SecurityController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.security.springsecurityproject.entity.User;
 import com.security.springsecurityproject.service.SecurityService;
 
+
 @RestController
 @RequestMapping("/users")
 public class SecurityController {
@@ -21,7 +22,7 @@ public class SecurityController {
 	@GetMapping("/allUsers")
 	public String getUsers()
 	{
-		return "Success";
+		return "Successfully done";
 	}
 	
 
